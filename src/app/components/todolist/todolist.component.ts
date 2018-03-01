8bf9679ae00aee26489b57bdc68abec690dede6d
yiluyanxia <yiluyanxia@163.com>
2018-03-01 13:59:06 +0800

    todolist

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
+  public todo:any='';
+  public list=[];
 
   constructor() { }
 
   ngOnInit() {
+  } 
+  addData(){
+    this.list.push(this.todo)
+  }
+  addTodo(e){
+
+    if(e.keyCode == 13){
+      let obj = {
+        todo :this.todo,
+        status:1
+      }
+      this.list.push(obj);
+      this.todo ='';
+    }
+  }
+  deleteData(index){
+    this.list.splice(index,1);
+  }
+  changeTodo(index){
+    this.list[index].status = 0
   }
 
 }
